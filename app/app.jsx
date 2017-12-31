852c8e62ac4dee3bb87786f89765ba68c7425919
Front-start <borodarox@gmail.com>
2017-12-31 10:46:15 +0300

    Url на gh-pages должен открывать главную

--- app/app.jsx
+++ app/app.jsx
@@ -14,6 +14,7 @@ ReactDOM.render(
     <div>
       <Nav />
       <Switch>
+        <Route exact path="/TODO-list" component={ItemList} />
         <Route exact path="/" component={ItemList} />
         <Route exact path="/item" component={Item} />
         <Route component={NotFound} />
